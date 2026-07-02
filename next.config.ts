5295691b1a5583836a09f76a92f98907ae138eb4
timeconsultingv <timeconsulting.v@gmail.com>
2026-07-02 14:56:37 +0700

    Fix profile image

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
